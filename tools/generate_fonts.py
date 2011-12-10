a628d4cd53d407f70454fa259190c2efcabb6fda
Aravinda <hallimanearavind@gmail.com>
2011-12-10 09:20:09 +0530

    Generated .otf instead of .ttf, added tools dir and python tools

---
+++ tools/generate_fonts.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Program reads the fontforge source file and generates otf font
+# Copyright (C) 2011  Aravinda VK <hallimanearavind@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import fontforge
+    
+font = fontforge.open(sys.argv[1])
+
+flags  = ("opentype", "dummy-dsig", "round")
+font_name = sys.argv[1].replace(".sfd",".otf")
+font.generate(font_name, flags=flags)
+font.close()
